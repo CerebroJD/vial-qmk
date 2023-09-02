ba77c473c4261a6219784084a5edc0b1ad541ffa
Justin Dowell <jdcerebro@gmail.com>
2023-09-02 14:55:50 -0300

    Adding previously functining Vial port from March into this new repo for merging

---
+++ keyboards/tkc/candybar/lefty_r3/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCC, 0x88, 0x3D, 0xC4, 0x99, 0x95, 0x68, 0xA1}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
