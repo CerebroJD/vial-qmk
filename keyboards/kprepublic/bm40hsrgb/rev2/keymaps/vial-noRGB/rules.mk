25e4e5f5ad26e3d17b02b8d73e8e7cd65285041c
Justin Dowell <jdcerebro@gmail.com>
2023-09-10 12:52:10 -0300

    Updated vial/rules.mk and config.h to strip out a bunch of RGB modes, limited tapdance and combos, disabled media and mouse keys in order to build small enough firmware. Added vial-noRGB directory and config to enable more tapdance, combos, media keys, and mouse keys but with RGB matrix fully disabled.

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/vial-noRGB/rules.mk
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/vial-noRGB/rules.mk
@@ -2,9 +2,9 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 LTO_ENABLE = yes
 
-RGBLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes
 BACKLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = yes             # Enable per-key coordinate based RGB effects. Do not enable with RGBlight
+RGB_MATRIX_ENABLE = no              # Enable per-key coordinate based RGB effects. Do not enable with RGBlight
 RGB_MATRIX_KEYPRESSES   = no        # Enable reactive per-key effects.
 RGB_MATRIX_FRAMEBUFFER_EFFECTS = no # Enable frame buffer effects like the typing heatmap.
 
@@ -12,5 +12,5 @@ KEY_OVERRIDE_ENABLE = no
 QMK_SETTINGS = yes
 TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
-MOUSEKEY_ENABLE = no #1280 bytes
-EXTRAKEY_ENABLE = no
+MOUSEKEY_ENABLE = yes #1280 bytes
+EXTRAKEY_ENABLE = yes
